9ca9c16da97dd03d1ff218a7e3235f33748468f5
u222471 <lukas.elsner@sbb.ch>
2017-01-25 11:52:42 +0100

    add more tags

--- conf/tags.lua
+++ conf/tags.lua
@@ -78,7 +78,7 @@ awful.screen.connect_for_each_screen(function(s)
     --set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({"[0]", "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]" }, s, awful.layout.layouts[2])
+    awful.tag({"[~]", "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]", "[0]", "[-]", "[=]", "[<-]", "[I]", "[D]"}, s, awful.layout.layouts[2])
 end)
 
 
